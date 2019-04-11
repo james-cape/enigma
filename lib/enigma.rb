247b66714973cd8a5d5eb04ad21797fefdada2fe
James Cape <james.cape@gmail.com>
2019-04-10 19:53:37 -0600

    Implement encrypt without scramble

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,22 +1,60 @@
 class Enigma
 
-  attr_reader :todays_date,
+  attr_reader :today,
               :random_key
 
   def initialize
     time = Time.new
-    year = time.year
-    month = time.month
-    day = time.day
-    @todays_date = month + day + year
+    @today = "#{time.month}#{time.day}#{time.year}".rjust(8,"0")
     @random_key = 5.times.map{rand(10)}.join
 
   end
 
 
-  def encrypt(message, key = random_key, date = todays_date)
-
+  def encrypt(message, key = random_key, date = today)
+    components = Hash.new(0)
+    components[:encryption] = message # Change later to encrypted string
+    components[:key] = key
+    components[:date] = date
+    components
   end
+
+
+  # 4 different shifts:
+  # 1st, 5th,  9th characters use A shift
+  # 2nd, 6th, 10th characters use B shift
+  # 3rd, 7th, 11th characters use C shift
+  # 4th, 8th, 12th characters use D shift
+
+  # Keys: from the 5-digit random_key, (i.e. 03827)
+  # A shift key (03)
+  # B shift key (38)
+  # C shift key (82)
+  # D shift key (27)
+
+  # Offsets: from the MMDDYY (i.e. 041019)
+  # Square the numeric form (1682558361)
+  # Take the last four digits (8361)
+  # A shift offset (8)
+  # B shift offset (3)
+  # C shift offset (6)
+  # D shift offset (1)
+
+  # A final shift (11)
+  # B final shift (41)
+  # C final shift (88)
+  # D final shift (28)
+
+
+
+
+
+
+  # Our algorithm will use the standard lowercase alphabet, plus a space, so 27 characters in total.
+  ############ ("a".."z").to_a << " " ############
+
+  # Finding the shifts... could be its own class?
+
 
 
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,12 +3,22 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
+require 'date'
+require 'pry'
 
 class EnigmaTest < Minitest::Test
 
   def test_enigma_exists
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
+  end
+
+  def test_encrypt_defaults_with_random_5_digit_key
+    enigma = Enigma.new
+
+    expected = 5
+    actual = enigma.encrypt("hello world")[:key].length
+    assert_equal expected, actual
   end
 
 
