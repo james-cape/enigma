2b666f8de2505d53664009d898e664fab8726a66
James Cape <james.cape@gmail.com>
2019-04-11 12:02:20 -0600

    Implement shift. Next will try to run four shifts on 11 characters

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,16 +7,61 @@ class Enigma
     time = Time.new
     @today = "#{time.month}#{time.day}#{time.year}".rjust(8,"0")
     @random_key = 5.times.map{rand(10)}.join
-
+    @raw_alphabet = ("a".."z").to_a << " "
   end
 
 
   def encrypt(message, key = random_key, date = today)
     components = Hash.new(0)
-    components[:encryption] = message # Change later to encrypted string
+
+    components[:encryption] = shift_message(message, key, date) # Change later to encrypted string
+    binding.pry
     components[:key] = key
     components[:date] = date
     components
+  end
+
+  def shift_message(message, key, date)
+    (0..message.size).step(4) do |i|
+       message[i] = a_shift(key, date)[@raw_alphabet.find_index(message[i])]
+     end
+    (1..message.size).step(4) do |i|
+       message[i] = b_shift(key, date)[@raw_alphabet.find_index(message[i])]
+     end
+    (2..message.size).step(4) do |i|
+       message[i] = c_shift(key, date)[@raw_alphabet.find_index(message[i])]
+     end
+
+     binding.pry
+    message
+  end
+
+  def last_four(date)
+    (date.to_i ** 2).to_s[-4..-1]
+  end
+
+  def a_shift(key, date)
+    shift_key = key[0..1].to_i
+    shift_offset = last_four(date)[0].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+  def b_shift(key, date)
+    shift_key = key[1..2].to_i
+    shift_offset = last_four(date)[1].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+  def c_shift(key, date)
+    shift_key = key[2..3].to_i
+    shift_offset = last_four(date)[2].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+  def d_shift(key, date)
+    shift_key = key[3..4].to_i
+    shift_offset = last_four(date)[3].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
   end
 
 
@@ -44,11 +89,6 @@ class Enigma
   # B final shift (41)
   # C final shift (88)
   # D final shift (28)
-
-
-
-
-
 
   # Our algorithm will use the standard lowercase alphabet, plus a space, so 27 characters in total.
   ############ ("a".."z").to_a << " " ############
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,11 +13,26 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  def test_encrypt_defaults_with_random_5_digit_key
+  def test_encrypt_defaults_with_random_5_digit_key_and_todays_date
     enigma = Enigma.new
+    time = Time.new
 
     expected = 5
     actual = enigma.encrypt("hello world")[:key].length
+    assert_equal expected, actual
+
+    # Possible spot for mock/stubs
+    expected = "#{time.month}#{time.day}#{time.year}".rjust(8,"0")
+    actual = enigma.encrypt("hello world")[:date]
+    assert_equal expected, actual
+  end
+
+  def test_encrypting_message
+    enigma = Enigma.new
+    time = Time.new
+
+    expected = 5
+    actual = enigma.encrypt("hello world")
     assert_equal expected, actual
   end
 
