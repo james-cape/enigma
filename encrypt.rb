5a39c22b8c7c47299f3d3c9c2ec74d6ecfe58465
James Cape <james.cape@gmail.com>
2019-04-13 09:28:25 -0600

    Begin accounting for \n

--- encrypt.rb
+++ encrypt.rb
@@ -1,31 +1,29 @@
 require './lib/enigma'
 require 'pry'
-# Takes two command line arguments
 
-# first is existing file to encrypt
-
-# second is a file to write the encryption to.
-
-#program should also output to the screen:
-# - file it wrote to
-# - key
-# - date
+enigma = Enigma.new
+tim = Time.new
 
 puts "Enter file name to encrypt: "
 
-raw_file = gets.chomp
+raw_file = File.read(gets.chomp)
 # message.txt
 
-encrypt(File.read(raw_file))
+binding.pry
+raw_file = enigma.encrypt(File.read(gets.chomp))
 
+# message.txt
+
+# encrypt(File.read(gets.chomp))
 
 
 puts "Enter the file name to write the encryption to: "
 
-encryption_file = gets.chomp
+# encryption_file = gets.chomp
 # encrypted.txt
 
+encrypted_file = File.new(gets.chomp)
 
+File.write(encrypted_file, raw_file)
 
-
-# print "Created 'encrypted.txt' with the key 82648 and date 240818"
+puts "Created 'encrypted.txt' with the key 82648 and date 240818"
