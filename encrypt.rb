fd2a30b4994e871b0e338b075f6d931b8ff1edff
James Cape <james.cape@gmail.com>
2019-04-12 21:53:18 -0600

    Imports an existing text file and reads the text contained within

--- encrypt.rb
+++ encrypt.rb
@@ -1,3 +1,5 @@
+require './lib/enigma'
+require 'pry'
 # Takes two command line arguments
 
 # first is existing file to encrypt
@@ -14,10 +16,16 @@ puts "Enter file name to encrypt: "
 raw_file = gets.chomp
 # message.txt
 
+encrypt(File.read(raw_file))
+
+
+
 puts "Enter the file name to write the encryption to: "
 
 encryption_file = gets.chomp
 # encrypted.txt
+
+
 
 
 # print "Created 'encrypted.txt' with the key 82648 and date 240818"
