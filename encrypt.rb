c97bc67f467dc6d6e255afac1e1d729e7270c8f2
James Cape <james.cape@gmail.com>
2019-04-13 10:01:27 -0600

    Account for \n in incoming message.txt files

--- encrypt.rb
+++ encrypt.rb
@@ -2,19 +2,26 @@ require './lib/enigma'
 require 'pry'
 
 enigma = Enigma.new
-tim = Time.new
+time = Time.new
 
 puts "Enter file name to encrypt: "
 
-raw_file = File.read(gets.chomp)
+# raw_file = gets.chomp
 # message.txt
 
-binding.pry
-raw_file = enigma.encrypt(File.read(gets.chomp))
+raw_lines = File.read(gets.chomp).split("\n")
+
+encrypted_text = ""
+encrypted_lines = raw_lines.each do |line|
+  encrypted_text += enigma.encrypt(line)[:encryption] + "\n"
+end
+encrypted_text.chomp!
+# text_lines = enigma.encrypt(File.read(gets.chomp)).split("\n")
 
 # message.txt
 
 # encrypt(File.read(gets.chomp))
+
 
 
 puts "Enter the file name to write the encryption to: "
@@ -24,6 +31,7 @@ puts "Enter the file name to write the encryption to: "
 
 encrypted_file = File.new(gets.chomp)
 
-File.write(encrypted_file, raw_file)
+File.write(encrypted_file, encrypted_text)
+# binding.pry
 
 puts "Created 'encrypted.txt' with the key 82648 and date 240818"
