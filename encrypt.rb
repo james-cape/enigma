1c2aab3934bfd534842e00d3d7c44f98c903f8d8
James Cape <james.cape@gmail.com>
2019-04-13 17:29:35 -0600

    Clean up

--- encrypt.rb
+++ encrypt.rb
@@ -2,7 +2,6 @@ require './lib/enigma'
 require 'pry'
 
 enigma = Enigma.new
-# time = Time.new
 
 puts "Enter file name to encrypt: "
 
@@ -10,31 +9,18 @@ puts "Enter file name to encrypt: "
 raw_lines = File.read(gets.chomp).split("\n")
 
 encrypted_text = ""
-encrypted_lines = raw_lines.each do |line|
+raw_lines.each do |line|
   encrypted_text += enigma.encrypt(line)[:encryption] + "\n"
 end
 encrypted_text.chomp!
-# text_lines = enigma.encrypt(File.read(gets.chomp)).split("\n")
-
-# message.txt
-
-# encrypt(File.read(gets.chomp))
-
 
 
 puts "Enter the file name to write the encryption to: "
 
-# encryption_file = gets.chomp
 # encrypted.txt
-
-
 encrypted_file_name = gets.chomp
 encrypted_file = File.open(encrypted_file_name, "w")
-# binding.pry
 
 encrypted_file.write(encrypted_text)
 
-# binding.pry
-
 puts "Created '#{encrypted_file_name}' with the key #{enigma.random_key} and date #{enigma.today}"
-# binding.pry
