85e0d98d765205364a190a6a95d52eb0083c00d6
James Cape <james.cape@gmail.com>
2019-04-13 23:26:34 -0600

    Add tests and methods to shift alphabet based on key/date

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -4,7 +4,23 @@ class Cipher
 
   def initialize
     @raw_alphabet = ("a".."z").to_a << " "
+    # @message = message
+    # @key = key
+    # @date = date
   end
+
+
+  def last_four(date)
+    (date.to_i ** 2).to_s[-4..-1]
+  end
+
+  def shift(key, date, position)
+    shift_key = key[position..(position + 1)].to_i
+    shift_offset = last_four(date)[position].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+
 
   def encrypt_shift(message, key, date, position)
     (position..message.size-1).step(4) do |i|
@@ -48,6 +64,35 @@ class Cipher
 
     message
   end
+
+
+
+  def a_shift(key, date)
+    shift_key = key[0..1].to_i
+    shift_offset = last_four(date)[0].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+  def b_shift(key, date)
+    shift_key = key[1..2].to_i
+    shift_offset = last_four(date)[1].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+  def c_shift(key, date)
+    shift_key = key[2..3].to_i
+    shift_offset = last_four(date)[2].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+  def d_shift(key, date)
+    shift_key = key[3..4].to_i
+    shift_offset = last_four(date)[3].to_i
+    @raw_alphabet.rotate(shift_key + shift_offset)
+  end
+
+
+
 
 
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -24,5 +24,42 @@ class CipherTest < Minitest::Test
     assert_equal expected, actual
   end
 
+  def test_cipher_starts_with_raw_alphabet
+    cipher = Cipher.new
+
+    expected = ("a".."z").to_a << " "
+    actual = cipher.raw_alphabet
+    assert_equal expected, actual
+  end
+
+  def test_cipher_extracts_last_four_from_date
+    cipher = Cipher.new
+
+    expected = "5561"
+    actual = cipher.last_four("130419")
+    assert_equal expected, actual
+  end
+
+  def test_cipher_creates_shifted_alphabet
+    cipher = Cipher.new
+
+    expected = ["k", "l", "m", "n", "o"]
+    actual = cipher.shift("05354", "130419", 0)[0..4]
+    assert_equal expected, actual
+  end
+
+  # def test_cipher_transforms_message
+  #   cipher = Cipher.new
+  #   # message = "tryout"
+  #   # key = 06590
+  #   # date = 130419
+  #
+  #   expected = "asb"
+  #   actual = cipher.encrypt_shift("tryout", "05643", "130419", 0)
+  #   assert_equal expected, actual
+  # end
+
+
+
 
 end
