6a1b6c537babace0a3de3939db6f5c5595ece45f
James Cape <james.cape@gmail.com>
2019-04-15 21:42:55 -0600

    Fix date input in crack method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -153,29 +153,50 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, actual
   end
 
+  def test_message_crack_encryption_with_a_date_and_different_string
+    cipher = Cipher.new
+    enigma = Enigma.new(cipher)
+    enigma.encrypt("this file is the one that should be encrypted end", "26132", "150419")
+
+    expected = {
+      encryption: "xtaydrarilayde kd fkde gxlknsfdjdnxfizvxbalkhlxth",
+      key: "26132",
+      date: "150419"
+     }
+    actual = enigma.encrypt("this file is the one that should be encrypted end", "26132", "150419")
+    assert_equal expected, actual
+    expected = {
+      decryption: "this file is the one that should be encrypted end",
+      date: "150419",
+      key: "26132"
+     }
+    actual = enigma.crack("xtaydrarilayde kd fkde gxlknsfdjdnxfizvxbalkhlxth", "150419")
+    assert_equal expected, actual
+  end
 
 
-  # def test_message_crack_encryption_without_date
-  #   skip
-  #   cipher = Cipher.new
-  #   enigma = Enigma.new(cipher)
-  #   enigma.encrypt("hello world end", "08304", "291018")
-  #
-  #   expected = {
-  #     encryption: "vjqtbeaweqihssi",
-  #     key: "08304",
-  #     date: "291018"
-  #    }
-  #   actual = enigma.encrypt("hello world end", "08304", "291018")
-  #   assert_equal expected, actual
-  #
-  #   expected = {
-  #     decryption: "hello world end",
-  #     date: "# todays date in the format DDMMYY",
-  #     key: "# key used for encryption"
-  #    }
-  #   actual = enigma.crack("vjqtbeaweqihssi")
-  #   assert_equal expected, actual
-  # end
+
+  def test_message_crack_encryption_without_date
+    
+    cipher = Cipher.new
+    enigma = Enigma.new(cipher)
+    enigma.encrypt("hello world end", "08304", "291018")
+
+    expected = {
+      encryption: "vjqtbeaweqihssi",
+      key: "08304",
+      date: "291018"
+     }
+    actual = enigma.encrypt("hello world end", "08304", "291018")
+    assert_equal expected, actual
+
+    expected = {
+      decryption: "hello world end",
+      date: "# todays date in the format DDMMYY",
+      key: "# key used for encryption"
+     }
+    actual = enigma.crack("vjqtbeaweqihssi")
+    assert_equal expected, actual
+  end
 
 end
