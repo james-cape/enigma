0219055d270be550c29f7a75ba4f74559ac8d649
James Cape <james.cape@gmail.com>
2019-04-16 17:34:08 -0600

    Adds .dup to message to prevent string mutilation

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -1,10 +1,17 @@
 class Cracker
-  attr_reader :range
+  attr_reader :keys
 
   def initialize
-    @range = 0..99999.to_a
+    @cipher = Cipher.new
+    @keys = (0..99999).to_a
+    @alphabet = ("a".."z").to_a << " "
+    @enigma = Enigma.new(@cipher)
   end
 
-
-
+  def brute_force(message, date = today)
+    @keys.find do |integer_key|
+      string_key = integer_key.to_s.rjust(5, "0")
+      @enigma.decrypt(message, string_key, date)[:decryption][-4..-1] == " end"
+    end.to_s.rjust(5, "0")
+  end
 end
