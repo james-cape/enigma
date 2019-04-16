f3def0d9eb2b6be60be9a6c44d8a7ecc680451d2
James Cape <james.cape@gmail.com>
2019-04-16 16:22:20 -0600

    Removes unnecessary shifter attributes

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -7,31 +7,15 @@ require './lib/shifter'
 class ShifterTest < Minitest::Test
 
   def test_shifter_exists
-    shifter = Shifter.new("12345", "140419", 1)
+    shifter = Shifter.new
 
     expected = Shifter
     actual = shifter
     assert_instance_of expected, actual
   end
 
-  def test_shifter_has_attributes
-    shifter = Shifter.new("12345", "140419", 1)
-
-    expected = "12345"
-    actual = shifter.key
-    assert_equal expected, actual
-
-    expected = "140419"
-    actual = shifter.date
-    assert_equal expected, actual
-
-    expected = 1
-    actual = shifter.slot
-    assert_equal expected, actual
-  end
-
   def test_last_four_of_square_of_date
-    shifter = Shifter.new("12345", "140419", 1)
+    shifter = Shifter.new
 
     expected = "5561"
     actual = shifter.last_four("140419")
@@ -39,12 +23,10 @@ class ShifterTest < Minitest::Test
   end
 
   def test_shift_alphabet_is_generated
-    shifter = Shifter.new("12345", "140419", 5)
+    shifter = Shifter.new
 
     expected = ["a", "b", "c", "d", "e"]
     actual = shifter.shift("12345", "140419", 5)[0..4]
     assert_equal expected, actual
   end
-
-
 end
