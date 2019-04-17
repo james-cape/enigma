074a7256638bb7d0aa6023f1c4fc3394caa8e1e7
James Cape <james.cape@gmail.com>
2019-04-16 21:01:41 -0600

    Modify to remain under 5 lines/method

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -7,20 +7,16 @@ class Cipher
   end
 
   def encrypt_slot(text, key, date, slot)
-    (slot..text.size-1).step(4) do |i|
-      if @alphabet.include?(text[i])
+    (slot..text.size-1).step(4) { |i| if @alphabet.include?(text[i])
         text[i] = @shifter.shift(key, date, slot)[@alphabet.find_index(text[i])]
-      end
-    end
+      end}
     text
   end
 
   def decrypt_slot(text, key, date, slot)
-    (slot..text.size-1).step(4) do |i|
-      if @alphabet.include?(text[i])
+    (slot..text.size-1).step(4) { |i| if @alphabet.include?(text[i])
         text[i] = @alphabet[@shifter.shift(key, date, slot).find_index(text[i])]
-      end
-    end
+      end}
     text
   end
 end
