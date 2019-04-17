c560441c894909f62848a8956f22f4bf3ebfd174
James Cape <james.cape@gmail.com>
2019-04-16 20:53:06 -0600

    Changes message to text

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,41 +10,41 @@ class Enigma
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt(message, key = random_key, date = today)
-    components = Hash.new(0)
-    components[:encryption] = encrypt_full_message(message.downcase.dup, key, date)
-    components[:key] = key
-    components[:date] = date
-    components
+  def encrypt(text, key = random_key, date = today)
+    results = Hash.new(0)
+    results[:encryption] = encrypt_text(text.downcase.dup, key, date)
+    results[:key] = key
+    results[:date] = date
+    results
   end
 
-  def decrypt(message, key = random_key, date = today)
-    components = Hash.new(0)
-    components[:decryption] = decrypt_full_message(message.downcase.dup, key, date)
-    components[:key] = key
-    components[:date] = date
-    components
+  def decrypt(text, key = random_key, date = today)
+    results = Hash.new(0)
+    results[:decryption] = decrypt_text(text.downcase.dup, key, date)
+    results[:key] = key
+    results[:date] = date
+    results
   end
 
-  def encrypt_full_message(message, key, date)
-    @cipher.encrypt_slot(message, key, date, 0)
-    @cipher.encrypt_slot(message, key, date, 1)
-    @cipher.encrypt_slot(message, key, date, 2)
-    @cipher.encrypt_slot(message, key, date, 3)
-    message
+  def encrypt_text(text, key, date)
+    @cipher.encrypt_slot(text, key, date, 0)
+    @cipher.encrypt_slot(text, key, date, 1)
+    @cipher.encrypt_slot(text, key, date, 2)
+    @cipher.encrypt_slot(text, key, date, 3)
+    text
   end
 
-  def decrypt_full_message(message, key, date)
-    @cipher.decrypt_slot(message, key, date, 0)
-    @cipher.decrypt_slot(message, key, date, 1)
-    @cipher.decrypt_slot(message, key, date, 2)
-    @cipher.decrypt_slot(message, key, date, 3)
-    message
+  def decrypt_text(text, key, date)
+    @cipher.decrypt_slot(text, key, date, 0)
+    @cipher.decrypt_slot(text, key, date, 1)
+    @cipher.decrypt_slot(text, key, date, 2)
+    @cipher.decrypt_slot(text, key, date, 3)
+    text
   end
 
   def crack(cipher_text, date = @today)
     key_value = @cracker.find_key(cipher_text, date)
-    decryption = decrypt_full_message(cipher_text, key_value, date)
+    decryption = decrypt_text(cipher_text, key_value, date)
     { decryption: decryption,
       date: date,
       key: key_value }
