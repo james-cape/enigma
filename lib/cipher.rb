bf9031471746544384a68d461bce8935b7d9f1d5
James Cape <james.cape@gmail.com>
2019-04-16 20:52:49 -0600

    Changes "message" to "text"

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -6,21 +6,21 @@ class Cipher
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt_slot(message, key, date, slot)
-    (slot..message.size-1).step(4) do |i|
-      if @alphabet.include?(message[i])
-        message[i] = @shifter.shift(key, date, slot)[@alphabet.find_index(message[i])]
+  def encrypt_slot(text, key, date, slot)
+    (slot..text.size-1).step(4) do |i|
+      if @alphabet.include?(text[i])
+        text[i] = @shifter.shift(key, date, slot)[@alphabet.find_index(text[i])]
       end
     end
-    message
+    text
   end
 
-  def decrypt_slot(message, key, date, slot)
-    (slot..message.size-1).step(4) do |i|
-      if @alphabet.include?(message[i])
-        message[i] = @alphabet[@shifter.shift(key, date, slot).find_index(message[i])]
+  def decrypt_slot(text, key, date, slot)
+    (slot..text.size-1).step(4) do |i|
+      if @alphabet.include?(text[i])
+        text[i] = @alphabet[@shifter.shift(key, date, slot).find_index(text[i])]
       end
     end
-    message
+    text
   end
 end
