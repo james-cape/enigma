d5af96284056b91ae568c2fc7111efa52c894081
James Cape <james.cape@gmail.com>
2019-04-16 20:06:38 -0600

    Adds time instance variable

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -2,13 +2,14 @@ class Cracker
   attr_reader :keys
 
   def initialize
+    @today = Time.now.strftime("%d%m%y")
     @cipher = Cipher.new
     @keys = (0..99999).to_a
     @alphabet = ("a".."z").to_a << " "
     @enigma = Enigma.new(@cipher)
   end
 
-  def find_key(message, date = today)
+  def find_key(message, date = @today)
     @keys.find do |integer_key|
       string_key = integer_key.to_s.rjust(5, "0")
       @enigma.decrypt(message, string_key, date)[:decryption][-4..-1] == " end"
